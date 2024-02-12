353f6b69315da03739d0e7912300b59b112f626e
Esubalew197 <esubalewmillion@gmail.com>
2024-02-13 01:35:53 +0300

    task 4

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,8 @@ indexRoute
   .get('/status', AppController.getStatus)
   .get('/stats', AppController.getStats)
   .post('/users', UsersController.postNew)
-  .get('/connect', AuthController.getConnect)
-  .get('/disconnect', AuthController.getDisconnect)
-  .get('/users/me', UsersController.getMe);
+  .get('/connect', handleAuthorization, AuthController.getConnect)
+  .get('/disconnect', handleXToken, AuthController.getDisconnect)
+  .get('/users/me', handleXToken, UsersController.getMe)
 
 export default indexRoute;
