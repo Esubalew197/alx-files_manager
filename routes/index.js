33ac35de4c9bfef33817692edc2fc56439e32a84
Esubalew197 <esubalewmillion@gmail.com>
2024-02-14 19:11:29 +0300

    update

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import UsersController from '../controllers/UsersController';
+import { handleAuthorization, handleXToken } from '../middlewares/auth.middleware';
+import FilesController from '../controllers/FilesController';
 
 const indexRoute = Router();
 
@@ -12,5 +14,11 @@ indexRoute
   .get('/connect', handleAuthorization, AuthController.getConnect)
   .get('/disconnect', handleXToken, AuthController.getDisconnect)
   .get('/users/me', handleXToken, UsersController.getMe)
+  .post('/files', handleXToken, FilesController.postUpload)
+  .get('/files/:id', handleXToken, FilesController.getShow)
+  .get('/files', handleXToken, FilesController.getIndex)
+  .get('/files/:id/data', FilesController.getFile)
+  .put('/files/:id/publish', handleXToken, FilesController.putPublish)
+  .put('/files/:id/unpublish', handleXToken, FilesController.putUnpublish);
 
 export default indexRoute;
